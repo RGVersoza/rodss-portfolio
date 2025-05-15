ed537a1e947ee8ad46097aba394b5b406a6bd921
Rodrigo Gem Versoza <rodrigogemversoza15@gmail.com>
2025-05-15 15:47:52 +0800

    feat: updating of  vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: process.env.VITE_BASE_PATH || "/rodss-portfolio",
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx','resources/js/pages/main.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
